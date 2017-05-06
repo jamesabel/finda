ccfcee307e928765b3c32d629cf18feb8f3fd504
james abel <j@abel.co>
2017-05-06 13:17:25 -0700

    use requirements.txt

--- make_venv.bat
+++ make_venv.bat
@@ -1,1 +1,5 @@
-c:\python34\python.exe C:\Python34\Tools\Scripts\pyvenv.py venv
+REM py2exe is apparently still only at Python 3.4
+c:\Python34\python.exe -m venv --clear venv
+venv\Scripts\pip3 install -U pip
+venv\Scripts\pip3 install -U setuptools
+venv\Scripts\pip3 install -r requirements.txt
