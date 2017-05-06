843f37b90e1b269ede7e87b6342da313fa79476d
james abel <j@abel.co>
2017-05-06 14:29:30 -0700

    add description

--- finda.py
+++ finda.py
@@ -1,16 +1,20 @@
 
 import argparse
+import finda
 import finda.finda
 import finda.license
 
 if __name__ == "__main__":
-    description = "Finds files that have the given string(s) in file name, path or text file contents.  If path(s) are not given all local disks are searched."
-    parser = argparse.ArgumentParser()
+    description = "Finds files that have the given string(s) in file name, path or simple text file contents. " \
+                  "Available via https://github.com/jamesabel/finda and/or www.abel.co. " \
+                  "Copyright 2013-2017 James Abel and licensed under MIT license (execute with -l for license information). " \
+                  "Version %s." % finda.__version__
+    parser = argparse.ArgumentParser(description=description)
     parser.add_argument('-s', '--strings', metavar='string', nargs='*', help='string(s) to search for - can contain wildcards or regexs')
     parser.add_argument('-e', '--exclude', metavar='string', default=[], required=False, nargs='*', help='string(s) to exclude in the search')
     parser.add_argument('-p', '--paths', metavar='path', default=['.'], nargs='*', help="paths to search - can include folders/directories and/or disks and contain wildcards")
     parser.add_argument('-c', '--case', action='store_true', help="take case into account, i.e. case sensitive (default is case insensitive)")
-    parser.add_argument('-l', '--license', action='store_true', help="print license info (finda is copyright James Abel)")
+    parser.add_argument('-l', '--license', action='store_true', help="print license info")
     parser.add_argument('-v', '--verbose', action='store_true', help="output status messages during execution")
     args = parser.parse_args()
 
